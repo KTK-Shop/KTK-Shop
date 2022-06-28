c6b8ce1d9248711f3a95ee511291a74e6b3cf16a
DKhoi02 <khoidndgcc200058@fpt.edu.vn>
2022-06-28 10:40:32 +0700

    rename HomepageController

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomePageController extends AbstractController
+class HomepageController extends AbstractController
 {
     /**
      * @Route("/homepage", name="app_home_page")
